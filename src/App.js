81ded03bc3489497d83a58d39efdf572de4b47bf
Intellidevelopers <134317370+Intellidevelopers@users.noreply.github.com>
2023-12-29 13:52:06 +0100

    h

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <><><><div className="arrow-left">
+      <svg
+        viewBox="0 0 1024 1024"
+        fill="currentColor"
+        height="1.5em"
+        width="1.5em"
+      >
+        <path d="M872 474H286.9l350.2-304c5.6-4.9 2.2-14-5.2-14h-88.5c-3.9 0-7.6 1.4-10.5 3.9L155 487.8a31.96 31.96 0 000 48.3L535.1 866c1.5 1.3 3.3 2 5.2 2h91.5c7.4 0 10.8-9.2 5.2-14L286.9 550H872c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8z" />
+      </svg>
+      <span>Instructions</span>
     </div>
+      <div className="naira">
+        Naira Referral Instructions
+      </div>
+    </>
+      <br />
+      <br />
+      <div>
+        <span className="emoji">👋Referring is rewarding 🎉</span>
+        <p> Invite your friends to join our community and earn exciting rewards. 🤩 For every successful
+          referral, you will earn generous of <b>₦400</b>💰</p>
+      </div>
+      <div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className="emoji">🏆 Earning has never been easier 📈</span>
+        <p> You will earn <b>₦400</b> when a user you reffered  purchases <b>Cheap Data</b>of up to <b>₦2500</b>🚀</p>
+      </div>
+      <div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className="emoji">🌟 Ready to cash in your rewards? 🤑</span>
+        <p> As a customer that loves JigiPay. You need to have done a total transaction of <b>₦2500</b> to be able to withdraw your earnings 😀</p>
+      </div>
+    </><div className="referral">
+        <small>Referral Code</small>
+        <h3>8088886823</h3>
+        <h5>Copy code</h5>
+      </div></>
   );
 }
 
